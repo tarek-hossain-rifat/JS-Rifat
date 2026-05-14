36a6a150fb467ade6f100a131c280090cef4f763
Tarek Hossain Rifat <tarekhossainrifatofficial019@gmail.com>
2026-05-15 00:30:43 +0600

    03_conversionOperation js file open and Do Code

---
+++ 03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "33";
+console.log(typeof score);
+
+let changedScore = Number(score);
+console.log(typeof changedScore);
+
+let isActive_In_Number = 1;
+console.log(typeof isActive_In_Number);
+
+let isActive_In_Boolean = Boolean(isActive_In_Number);
+console.log("Data type is " + typeof isActive_In_Boolean + " and value is " +isActive_In_Boolean);
